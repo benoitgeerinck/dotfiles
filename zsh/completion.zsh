577c85278970374148c09712bffd7ae368eca8df
Benoit Geerinck <benoitgeerinck@gmail.com>
2019-06-28 13:34:07 +0200

    add ansible completions

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,12 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+antigen bundle zsh-users/zsh-completions
+
+autoload bashcompinit
+bashcompinit
+for file in $ZSH/ansible/*-completion.bash;
+do
+  source $file
+done
