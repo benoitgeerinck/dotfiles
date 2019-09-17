1a3e98e07f0db1e3dd91faa4d13887b982e330fc
benoitgeerinck <benoit.geerinck@kunstmaan.be>
2019-09-17 10:21:53 +0200

    add terraform aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,3 +4,9 @@ alias cls='clear' # Good 'ol Clear Screen command
 alias pa='ps aux'
 alias df='df -h'
 alias diff='colordiff'
+
+# Terraform
+alias teri='terraform init'
+alias terp='terraform plan'
+alias tera='terraform apply'
+alias terd='terraform destroy'
